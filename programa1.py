95bc80f384c18cd3d24a781a1fa5f0abddf0f392
Archivo Nuestro <archivonuestro.nosv@gmail.com>
2026-03-04 05:33:41 -0500

    Agregar programa1.py en rama programador (versión simple)

---
+++ programa1.py
@@ -0,0 +1,13 @@
+# programa1.py
+# Programa que incrementa un día a una fecha dada (versión simple)
+
+print("Programa 1: Incrementar un día a una fecha")
+
+dia = int(input("Ingrese el día: "))
+mes = int(input("Ingrese el mes: "))
+año = int(input("Ingrese el año: "))
+
+# Incrementar un día (versión simple, sin validar meses)
+dia += 1
+
+print(f"La fecha incrementada en un día es: {dia}/{mes}/{año}")
